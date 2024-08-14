4b515c98861f44f5d195877eeb37e759fb24ec25
Ricardo Albuquerque <162588790+keydnss@users.noreply.github.com>
2024-08-13 21:28:59 -0300

    Add files via upload

---
+++ iniciante/exercicio01.java
@@ -0,0 +1,15 @@
+//Calculando a área de um trapézio
+
+public class Main25{
+    public static void main(String[] args){
+        int b,B,h;
+        float area;
+        b = 5;
+        B = 4;
+        h = 5;
+        area = (float) (b+B)/2*h;
+
+        System.out.printf("Um trapézio com os valores:\nB = 8.0m\nb = 6.0m\nh = 5.0m\nPossui uma área de: %.2f", area);
+
+    }
+}

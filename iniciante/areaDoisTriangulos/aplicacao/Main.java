3667e930ebdcf8eb2f75c9109e6679a282dc5857
Ricardo Albuquerque <162588790+keydnss@users.noreply.github.com>
2024-12-16 17:02:39 -0300

    Add files via upload

--- iniciante/areaDoisTriangulos/aplicacao/Main.java
+++ iniciante/areaDoisTriangulos/aplicacao/Main.java
@@ -20,13 +20,10 @@ public class Main {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2;
-        double areaX = Math.sqrt( p * (p - x.a) * (p - x.b) * (p - x.c));
+        double areaX = x.area();
+        double areaY = y.area();
 
-        p = (y.a + y.b + y.c) / 2;
-        double areaY = Math.sqrt( p * (p - y.a) * (p - y.b) * (p - y.c));
-
-        System.out.println("Área do triângulo X: " + areaX);
+        System.out.println("\nÁrea do triângulo X: " + areaX);
         System.out.println("Área do triângulo Y: " + areaY);
 
         if (areaX > areaY) {

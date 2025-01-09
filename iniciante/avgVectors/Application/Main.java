f02327c726bcaac41d2d878a935f93225f336e91
Ricardo Albuquerque <162588790+keydnss@users.noreply.github.com>
2025-01-09 16:03:00 -0300

    Add files via upload

---
+++ iniciante/avgVectors/Application/Main.java
@@ -0,0 +1,29 @@
+package Application;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double [] vect = new double[3];
+
+        for(int i = 0; i<n ; i++){
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0.0;
+
+        for(int i = 0; i<n ; i++){
+            sum += vect[i];
+        }
+
+        double avg = sum/n;
+
+        System.out.printf("Avarege rate: %.2f\n", avg);
+
+        sc.close();
+    }
+}
